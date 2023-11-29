ea17914f02ab130d3a0c79a3e947ce1c01e8a7a0
nisrinawafaz <nisrinawafaz@gmail.com>
2023-11-29 12:17:35 +0700

    routes barang nota dan nota

---
+++ api/routes/barangNotaRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const barangNotaController = require('../controllers/barangNotaController');
+
+
+
+module.exports = router;
---
+++ api/routes/notaRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const notaController = require('../controllers/notaController');
+
+
+
+module.exports = router;
